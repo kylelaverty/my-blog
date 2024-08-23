658537012146d0305bc15e786b155327556d7374
Kyle Laverty <k.laverty@gmail.com>
2024-08-23 11:58:32 -0400

    Temp fix to get working locally
    Which using the custom site, local testing no longer works.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
+//import { defineConfig } from "astro/config";
+
+//export default defineConfig({
+//  site: "https://blog.kylelaverty.ca",
+//});
+
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://blog.kylelaverty.ca",
+  site: "https://astro-nano-demo.vercel.app",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
