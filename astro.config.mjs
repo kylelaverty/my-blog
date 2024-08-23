f3f349a138c32de2825a76043f29d69a46b51690
Kyle Laverty <k.laverty@gmail.com>
2024-08-23 11:41:56 -0400

    Move to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://kylelaverty.github.io",
-  base: "/my-blog",
+  site: "https://blog.kylelaverty.ca",
 });
